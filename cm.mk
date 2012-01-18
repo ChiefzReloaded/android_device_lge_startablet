a85064ba257dbee1021057fa989f0d761e3259a4
Ryan Scott <chiefz.reloaded@gmail.com>
2012-01-18 01:01:17 -0800

    fixed device name issue

--- cm.mk
+++ cm.mk
@@ -12,7 +12,7 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 #
 PRODUCT_NAME := cm_startablet
 PRODUCT_BRAND := lge
-PRODUCT_DEVICE := v909
+PRODUCT_DEVICE := startablet
 PRODUCT_MODEL := LG-V909
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=lge_startablet BUILD_ID=HMJ37 BUILD_FINGERPRINT=lge/lge_startablet/v909:3.1/HMJ37/v10o.41FD770E:user/release-keys PRIVATE_BUILD_DESC="startablet-user 3.1 HMJ37 release-keys"
