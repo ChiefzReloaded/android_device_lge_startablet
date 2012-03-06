a9cceb141aab13a675be2f86f606878f324642b3
Ryan Scott <chiefz.reloaded@gmail.com>
2012-03-05 20:33:55 -0800

    added a missing file to fix the pull

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,11 +19,11 @@
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Allow compatibility with old touchscreens
-BOARD_USE_LEGACY_TOUCHSCREEN := true
+#BOARD_USE_LEGACY_TOUCHSCREEN := false
 
 # Camera Setup
 USE_CAMERA_STUB := false
-BOARD_FIRST_CAMERA_FRONT_FACING := true
+COMMON_GLOBAL_CFLAGS += -DREVERSE_FFC_MIRROR_LOGIC
 
 -include vendor/lge/startablet/BoardConfigVendor.mk
 
@@ -37,8 +37,13 @@ TARGET_CPU_SMP := true
 #TARGET_HAVE_TEGRA_ERRATA_657451 := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
+#TARGET_SPECIFIC_HEADER_PATH := device/lge/startablet/include
+
 # Custom Recovery UI Settings
-#BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/lge/startablet/recovery/recovery_ui.c
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/lge/startablet/recovery/recovery_ui.c
+
+# Override recovery init.rc
+TARGET_RECOVERY_INITRC := device/lge/startablet/recovery/init.rc
 
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_HAS_LARGE_FILESYSTEM := true
@@ -48,7 +53,7 @@ TARGET_BOOTLOADER_BOARD_NAME := startablet
 
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_CMDLINE := nvmem=128M@384M mem=1024M@0M vmalloc=256M video=tegrafb console=none usbcore.old_scheme_first=1 lp0_vec=8192@0x1f855000 tegra_fbmem=3937280@0x1f86b000 tegraboot=sdmmc muic_path=0:15 usb_serial=028841C541E0C297 usb_mode=0 hw_rev=Rev_1_3 gpt
-BOARD_PAGE_SIZE := 2048
+BOARD_PAGE_SIZE := 0x00000800
 
 BOARD_EGL_CFG := device/lge/startablet/egl.cfg
 
@@ -56,7 +61,8 @@ USE_OPENGL_RENDERER := true
 
 TARGET_HAVE_HDMI_OUT := true
 
-BOARD_USES_AUDIO_LEGACY := true
+#BOARD_USES_AUDIO_LEGACY := true
+BOARD_PREBUILT_LIBAUDIO := true
 
 BOARD_USE_USB_MASS_STORAGE_SWITCH := true
 
@@ -69,11 +75,13 @@ BOARD_FLASH_BLOCK_SIZE := 4096
 
 TARGET_PREBUILT_KERNEL := device/lge/startablet/kernel
 
+BOARD_UMS_LUNFILE := /sys/devices/platform/usb_mass_storage/lun0/file
+
 # Wifi related defines
 WPA_SUPPLICANT_VERSION := VER_0_8_X
-BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_DRIVER := WEXT
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
-BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_DRIVER := WEXT
 BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
 BOARD_WLAN_DEVICE := bcmdhd
 
@@ -83,15 +91,29 @@ WIFI_DRIVER_FW_PATH_AP  := "/system/vendor/firmware/fw_bcmdhd_apsta.bin"
 WIFI_DRIVER_MODULE_NAME := "bcmdhd"
 WIFI_DRIVER_MODULE_ARG  := "firmware_path=/system/vendor/firmware/fw_bcmdhd.bin nvram_path=/system/etc/wifi/nvram.txt"
 WIFI_BAND := 802_11_BG
+#WIFI_DRIVER_HAS_LGE_SOFTAP	:= true
 
 # Setting this to avoid boot locks on the system from using the "misc" partition.
 BOARD_HAS_NO_MISC_PARTITION := true
 
-# Indicate that the board has an Internal SD Card
-BOARD_HAS_SDCARD_INTERNAL := true
+BOARD_USES_HW_MEDIARECORDER := true
+BOARD_USES_HW_MEDIAPLUGINS := true
+
+BOARD_USE_SCREENCAP := true
+
+#BOARD_USES_LGE_HDMI_ROTATION := true
+
+BOARD_MOBILEDATA_INTERFACE_NAME := "vsnet0"
+
+TARGET_ELECTRONBEAM_FRAMES := 20
+
+TARGET_USES_GL_VENDOR_EXTENSIONS := true
+
+BOARD_FORCE_STATIC_A2DP := true
 
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
 
-BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk0p4
-BOARD_SDCARD_DEVICE_INTERNAL := /dev/block/mmcblk0p4
+TARGET_OVERLAY_ALWAYS_DETERMINES_FORMAT := true
+
+TARGET_DONT_SET_AUDIO_AAC_FORMAT := true
